fcab0f477b2693cae0c2ba6ad9d31e1580f1e3fc
Kapilan <mahenthirarajahk@gmail.com>
2020-03-26 15:33:05 +0100

    ajout d'un controller Annonce

--- src/main/java/org/csid/autobody/security/SecurityConfiguration.java
+++ src/main/java/org/csid/autobody/security/SecurityConfiguration.java
@@ -14,6 +14,8 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.sql.DataSource;
 
@@ -37,6 +39,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll()
                 .antMatchers("/h2-console/**").permitAll()
                 .antMatchers("/users").permitAll()
+                .antMatchers("/annonces").permitAll()
                 .anyRequest().authenticated().and().csrf().disable();
 
         auth.headers().frameOptions().sameOrigin();
