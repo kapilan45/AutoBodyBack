ecd6671605b7592113cb23bbee7a8bfa086a0943
Kapilan <mahenthirarajahk@gmail.com>
2020-06-19 16:04:02 +0200

    debug

---
+++ src/main/java/org/csid/autobody/specifications/AnnonceSpecifications.java
@@ -0,0 +1,48 @@
+package org.csid.autobody.specifications;
+
+import org.csid.autobody.entity.AnnonceEntity;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+public class AnnonceSpecifications implements Specification<AnnonceEntity> {
+
+    private SearchCriteria criteria;
+
+    public AnnonceSpecifications(SearchCriteria searchCriteria) {
+        this.criteria = searchCriteria;
+    }
+
+    @Override
+    public Predicate toPredicate(Root<AnnonceEntity> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
+
+        if (criteria.getOperation().equalsIgnoreCase(">")) {
+            return builder.greaterThanOrEqualTo(
+                    root.<String> get(criteria.getKey()), criteria.getValue().toString());
+        }
+        else if (criteria.getOperation().equalsIgnoreCase("<")) {
+            return builder.lessThanOrEqualTo(
+                    root.<String> get(criteria.getKey()), criteria.getValue().toString());
+        }
+        else if (criteria.getOperation().equalsIgnoreCase(":")) {
+            if (root.get(criteria.getKey()).getJavaType() == String.class) {
+                return builder.like(
+                        root.<String>get(criteria.getKey()), "%" + criteria.getValue() + "%");
+            } else {
+                return builder.equal(root.get(criteria.getKey()), criteria.getValue());
+            }
+        }
+        return null;
+    }
+
+    public SearchCriteria getCriteria() {
+        return criteria;
+    }
+
+    public void setCriteria(SearchCriteria criteria) {
+        this.criteria = criteria;
+    }
+}
---
+++ src/main/java/org/csid/autobody/specifications/AnnoncesSpecificationsBuilder.java
@@ -0,0 +1,60 @@
+package org.csid.autobody.specifications;
+
+import org.csid.autobody.entity.AnnonceEntity;
+import org.csid.autobody.entity.CategoryEntity;
+import org.csid.autobody.entity.MakeEntity;
+import org.csid.autobody.entity.ModelEntity;
+import org.csid.autobody.repository.CategoryRepository;
+import org.csid.autobody.repository.MakeRepository;
+import org.csid.autobody.repository.ModelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AnnoncesSpecificationsBuilder {
+
+    @Autowired
+    private MakeRepository makeRepository;
+    private ModelRepository modelRepository;
+    private CategoryRepository categoryRepository;
+
+    private final List<SearchCriteria> params;
+
+    public AnnoncesSpecificationsBuilder() {
+        params = new ArrayList<SearchCriteria>();
+    }
+
+    public AnnoncesSpecificationsBuilder with(String key, String operation, Object value) {
+        params.add(new SearchCriteria(key, operation, value));
+        return this;
+    }
+
+    public Specification<AnnonceEntity> build() {
+        if (params.size() == 0) {
+            return null;
+        }
+
+        List<Specification> specs = params.stream()
+                .map(AnnonceSpecifications::new)
+                .collect(Collectors.toList());
+
+        Specification result = specs.get(0);
+
+        for (int i = 0; i < params.size(); i++) {
+            result = params.get(i)
+                    .isOrPredicate()
+                    ? Specification.where(result)
+                    .and(specs.get(i))
+                    : Specification.where(result)
+                    .and(specs.get(i));
+        }
+        return result;
+    }
+
+    public List<SearchCriteria> getParams() {
+        return params;
+    }
+}
